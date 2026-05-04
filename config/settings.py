64a8e25265389fcc1dcdbb22fac1291372c70ef6
OviO17 <oviea4@gmail.com>
2026-05-04 21:23:23 +0100

    Use DATABASE_URL for production database

--- config/settings.py
+++ config/settings.py
@@ -1,4 +1,5 @@
 import os
+import dj_database_url
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -50,14 +51,9 @@ TEMPLATES = [
 WSGI_APPLICATION = "config.wsgi.application"
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "restaurant_db",
-        "USER": "postgres",
-        "PASSWORD": "fullstack",
-        "HOST": "localhost",
-        "PORT": "5432",
-    }
+    "default": dj_database_url.config(
+        default=os.environ.get("DATABASE_URL")
+    )
 }
 
 AUTH_PASSWORD_VALIDATORS = [
