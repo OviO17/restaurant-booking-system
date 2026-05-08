fc08121c320d7514d5006378265c6aa50bd42eed
OviO17 <oviea4@gmail.com>
2026-05-08 16:06:35 +0100

    Fix settings syntax error

--- config/settings.py
+++ config/settings.py
@@ -8,7 +8,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY", "django-insecure-local-dev-key")
 
 DEBUG = os.environ.get("DEBUG", "False") == "True"
 
-<!-- ALLOWED_HOSTS = ["localhost", "127.0.0.1", ".onrender.com"] -->
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", ".onrender.com"]
 
 INSTALLED_APPS = [
     "django.contrib.admin",
